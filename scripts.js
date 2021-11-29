c55354c9e99be3e16025625b2fafba6c010af1ce
Polo <lg550055@hotmail.com>
2021-11-28 21:41:47 -0600

    Adds repeated color validation and user prompt for number of logos in lower section

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function name() {
   let userName = prompt('What is your name? ');
-  document. write('Hello ' + userName);
+  document.write('Hello ' + userName);
 }
 
 function setColor() {
@@ -10,22 +10,33 @@ function setColor() {
   const w3c = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
   
   if (!w3c.includes(myColor)) {
-    myColor = prompt('Which valid w3c color would you like?');
+    while (!w3c.includes(myColor)) {
+      myColor = prompt('Which valid w3c color would you like?');
+    }
+    
   } else {
     document.body.style.backgroundColor = `${myColor}`;
   }
 }
 
-function restore(){
+function original(){
   document.body.style.backgroundColor = "#10E6C5";
 }
 
 function azure() {
-  setTimeout(function(){
-    let color = confirm('Want to swith background to azure?');
+  let azure = confirm('Want to switch background to azure?');
   
-    if (color) {
-      document.body.style.backgroundColor = "azure";
-    }
-  }, 1000);
+  if (azure) {
+    document.body.style.backgroundColor = "azure";
+  }
+}
+
+function logos() {
+  const n = prompt('How many logos: 1-5?')
+  for (let i=0; i<n; i++) {
+    let img = document.createElement('img');
+    img.src = "ghlogo.png";
+    img.height = "90";
+    document.getElementById("s2").appendChild(img);
+  }
 }
